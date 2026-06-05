5601ce8f994738a4bc22895baaa56e21f63aba49
Kiralyrobi <rgyebnar22@gmail.com>
2026-06-05 08:45:36 +0200

    péntek

---
+++ VisitingCard2Solution/ClassLibrary1Visiting/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1Visiting
+{
+    public class VisitingCardCreator
+    {
+        public void PrintVisitingCard(string name, DateTime dob, string street, string postcode, string city, string designation)
+        {
+            TimeSpan timeSpan = DateTime.Now - dob;
+            int totalDays = (int)timeSpan.TotalDays;
+
+            string lastName = name.Split(' ')[name.Split(' ').Length - 1];
+
+            Console.WriteLine();
+            Console.WriteLine("******** Output ********");
+            Console.WriteLine();
+            Console.WriteLine($"Hello {lastName} Here is your visiting card");
+            Console.WriteLine("------------------------------------------------------");
+            Console.WriteLine(name);
+            Console.WriteLine(designation);
+            Console.WriteLine(street);
+            Console.WriteLine($"{postcode} {city}");
+            Console.WriteLine($"Born: {totalDays} Days ago");
+            Console.WriteLine("------------------------------------------------------");
+        }
+    }
+}
---
+++ VisitingCard2Solution/VisitingCard2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1Visiting;
+
+namespace VisitingCard2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Welcome to Gotham's Best Visiting Card Maker*****");
+
+            Console.Write("Your Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("DOB(dd-MM-yyyy): ");
+            DateTime dob = DateTime.ParseExact(Console.ReadLine(), "dd-MM-yyyy", CultureInfo.InvariantCulture);
+
+            Console.Write("Street Address: ");
+            string street = Console.ReadLine();
+
+            Console.Write("PostCode: ");
+            string postcode = Console.ReadLine();
+
+            Console.Write("City: ");
+            string city = Console.ReadLine();
+
+            Console.Write("Your speciality: ");
+            string designation = Console.ReadLine();
+
+            VisitingCardCreator card = new VisitingCardCreator();
+            card.PrintVisitingCard(name, dob, street, postcode, city, designation);
+
+            Console.ReadKey();
+        }
+    }
+}
